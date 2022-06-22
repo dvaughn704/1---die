02fbe6bb8179deb2807ba919cfaeb11148b596a5
Don <107230967+dvaughn704@users.noreply.github.com>
2022-06-22 03:16:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+
+random_side1 = random.randint(1, 6)
+ 
+random_side2 = random.randint(1, 6)
+
+if random_side1 == 1:
+  print("1")
+elif random_side1 == 2:
+  print("2")
+elif random_side1 == 3:
+  print("3")
+elif random_side1 == 4:
+  print("4")
+elif random_side1 == 5:
+  print("5")
+else:
+  print("6")
+  
